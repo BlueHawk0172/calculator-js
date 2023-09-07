f8f95b09e4096c87fd2c590bff0befcba2993341
BlueHawk0172 <isbell.z13@gmail.com>
2023-09-07 19:33:11 -0400

    almost done

---
+++ script.js
@@ -0,0 +1,14 @@
+displayValue = "";
+function calculate(){
+    displayValue = eval(displayValue);
+    document.querySelector('#display').value = displayValue
+
+}
+function handleConcat(value){
+    displayValue+=value;
+    document.querySelector('#display').value = displayValue
+}
+function clearDisplay(){
+    displayValue=""
+    document.querySelector('#display').value = displayValue;
+}
